836e730c8ef8bf43859da5d368be7359f89f25ff
Chars Volta <charsvolta@gmail.com>
2026-01-18 14:13:39 -0300

    Add initial movies data structure

---
+++ movies.js
@@ -0,0 +1,10 @@
+export const movies = [
+  {
+    id: "inception",
+    title: "Inception",
+    year: 2010,
+    genre: ["Sci-Fi", "Thriller"],
+    actors: ["leonardo-dicaprio"],
+    watched: true
+  }
+];
