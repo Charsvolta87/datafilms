f604d1437b25128ec763fb89f425486091accf6b
Chars Volta <charsvolta@gmail.com>
2026-01-18 19:59:33 -0300

    grg

--- js/views/home.view.js
+++ js/views/home.view.js
@@ -1,18 +1,25 @@
 import { getCatalog } from "../data/catalog.service.js";
 import { createCard } from "../components/card.js";
-
+import { renderFilters, applyFilters } from "../components/filters.js";
+import { renderSortButton, applySort } from "../components/sort.js";
 
 export async function renderHome(container) {
-  const titles = await getCatalog();
+  const data = await getCatalog();
 
   container.innerHTML = `
-    <h2>Últimos agregados</h2>
+    <h2>Catálogo</h2>
+    ${renderSortButton()}
+    ${renderFilters()}
     <div class="grid" id="homeGrid"></div>
   `;
 
   const grid = document.getElementById("homeGrid");
 
-  titles.slice(0, 10).forEach(item => {
-    grid.appendChild(createCard(item));
-  });
+  function render(list) {
+    grid.innerHTML = "";
+    list.forEach(item => grid.appendChild(createCard(item)));
+  }
+
+  applySort(data, render);
+  applyFilters(data, render);
 }
